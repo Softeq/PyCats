d7edbb90daa2eb70ca10f5113b0782b61310af6b
Maksim Rodin <maksimrodin@gmail.com>
2020-06-29 19:51:10 +0300

    Add new lines

--- common/facade/api/__init__.py
+++ common/facade/api/__init__.py
@@ -4,5 +4,3 @@ from common._rest_qa_api.base_endpoint import BaseEndpoint, endpoint_factory, Ba
 from common._rest_qa_api import rest_checkers
 
 from common._rest_qa_api.rest_utils import SKIP, scaf_dataclass
-
-
